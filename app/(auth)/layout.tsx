5a17f91073e36c3c200a2cd969725d90935ecb45
pasindu gayan <pasindu9225@gmail.com>
2025-04-11 09:16:09 +0530

    create routes, auth pages.

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const authlayout = ({ children }: { children: React.ReactNode }) => {
+  return <div className=" auth-layout">{children}</div>;
+};
+
+export default authlayout;
