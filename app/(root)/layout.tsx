82e948e94a9061a2a5e60d016059d48449cd5968
pasindu gayan <pasindu9225@gmail.com>
2025-04-11 10:05:38 +0530

    srart implement home page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 
 const Rootlayout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div className="root-layout">
+      <nav>
+        <Link href="/" className=" flex items-center gap-2">
+          <Image src="/logo.svg" alt="logo" width={24} height={24} />
+          <h2 className=" text-primary-100">PrepWise</h2>
+        </Link>
+      </nav>
+      {children}
+    </div>
+  );
 };
 
 export default Rootlayout;
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,47 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const page = () => {
-  return <div>home page</div>;
+  return (
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>
+            Get ready to interview like a pro with AI powered interview platform
+          </h2>
+          <p>
+            Practice your interview skills with our AI powered interview
+            platform. Simulate real-time interviews and get feedback on your
+            performance.
+          </p>
+          <Button asChild className="btn-primary">
+            <Link href="/interview">Get Started</Link>
+          </Button>
+        </div>
+        <Image
+          src="/robot.png"
+          alt="robot"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
+      </section>
+      <section className="flex flex-col gap-4 mt-8">
+        <h2>Your interviews</h2>
+        <div className="interviews-section">
+          <p>You haven&apos;t scheduled any interviews yet </p>
+        </div>
+      </section>
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Take an Interview</h2>
+        <div className="interviews-section">
+          <p>There are no interviews available</p>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default page;
